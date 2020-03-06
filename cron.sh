391b8b8ef71d93c939f0dedd43f9a3d832954d33
Rajesh Pulabala <rajesh.pulabala@external.milliporesigma.com>
2020-03-06 14:40:00 -0600

    adding company certs to java trust store

--- cron.sh
+++ cron.sh
@@ -4,5 +4,40 @@
 
 # first open the cron file by using crontab -e 
 
+
+installing cron in ubuntu
+first check cron is installed or not. 
+
+dpkg -l cron
+
+dpkg-query: no packages found matching cron
+
+no cron
+
+lets install cron now
+
+apt-get update
+apt-get install cron -y 
+
+Desired=Unknown/Install/Remove/Purge/Hold
+| Status=Not/Inst/Conf-files/Unpacked/halF-conf/Half-inst/trig-aWait/Trig-pend
+|/ Err?=(none)/Reinst-required (Status,Err: uppercase=bad)
+||/ Name                                         Version                     Architecture                Description
++++-============================================-===========================-===========================-==============================================================================================
+ii  cron                                         3.0pl1-128+deb9u1           amd64                       process scheduling daemon
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # then add the sceduling timings to that file like
 # 2 * * * * root /root/script.sh         --------> this will run for every 2minuits
